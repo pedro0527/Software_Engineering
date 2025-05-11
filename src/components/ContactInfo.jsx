11e553b59f98313db391dc75ba26a3b0890751ab
pedro0527 <sooyong0527@naver.com>
2025-05-12 00:40:25 +0900

    Feat : 연락처 페이지 수정

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const ContactInfo = () => {
   return (
     <div className="contact-info">
-      <p>이메일: leesh914@naver.com</p>
-      <p>전화번호: 010-5559-****</p>
+      <p>이메일: sooyong0527@naver.com</p>
+      <p>전화번호: 010-6300-****</p>
       <p>주소: 동국대학교 신공학관</p>
     </div>
   );
