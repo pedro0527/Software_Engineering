8b44c25310b922bcc9feac485cff82f21df6bcaf
pedro0527 <sooyong0527@naver.com>
2025-05-12 00:55:42 +0900

    Design: 프로필 이미지 추가

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import doraImage from "../assets/dora.jpg";
+import profile from "../assets/profile.png";
 
 const ProfileCard = () => {
   return (
     <div className="profile">
-      <img src={doraImage} alt="프로필" />
+      <img src={profile} alt="프로필" />
       <div className="profile-info">
-        <h3>이름</h3>
-        <p>안녕하세요! 저는 React를 좋아하는 개발자입니다.</p>
+        <h3>이름 : 이수용</h3>
+        <p>안녕하세요! 저는 프론트엔드 개발자를 희망합니다.</p>
       </div>
     </div>
   );
 };
 
-export default ProfileCard; 
+export default ProfileCard;
